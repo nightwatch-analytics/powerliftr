622b8918b753dda61a25cac03196a602d69ab63a
Joseph Mohr <jmohr@thenightwatch.co>
2025-01-12 16:54:48 -0600

    add documenatation to `epley_1rm()`

--- R/epley_1rm.R
+++ R/epley_1rm.R
@@ -1,3 +1,14 @@
+#' Calculate one-repition maximum using the Epley formula
+#'
+#' @param weight The total amount of weight lifted.
+#' @param reps The number of repetitions completed.
+#'
+#' @returns The estimated one-rep max.
+#' @export
+#'
+#' @examples
+#' # 135 lbs lifted 10 times results in an estimated one-rep max of 180 lbs
+#' epley_1rm(weight = 135, reps = 10)
 epley_1rm <- function(weight, reps) {
   if (reps == 1) {
     weight
